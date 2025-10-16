22046af421ab264a3e5d3001a93364f0daa512a3
hiroshimamsugimoto-cmd <hiroshima.m.sugimoto@gmail.com>
2025-10-16 09:48:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,19 +53,19 @@ col1, col2, col3 = st.columns([2, 1, 1])
 with col1:
     開始日 = st.date_input("日付", key="start_date")
 with col2:
-    開始時 = st.number_input("時", min_value=0, max_value=23, value=9, key="start_hour")
+    開始時 = st.text_input("時", value="9", key="start_hour")
 with col3:
-    開始分 = st.number_input("分", min_value=0, max_value=59, value=0, key="start_minute")
+    開始分 = st.text_input("分", value="0", key="start_minute")
 
 # --- 終了日時 ---
 st.subheader("終了日時")
 col4, col5, col6 = st.columns([2, 1, 1])
 with col4:
-    終了日 = st.date_input("日付 ", key="end_date")
+    終了日 = st.date_input("日付", key="end_date")
 with col5:
-    終了時 = st.number_input("時 ", min_value=0, max_value=23, value=10, key="end_hour")
+    終了時 = st.text_input("時", value="10", key="end_hour")
 with col6:
-    終了分 = st.number_input("分 ", min_value=0, max_value=59, value=0, key="end_minute")
+    終了分 = st.text_input("分", value="0", key="end_minute")
 
 # --- 測定値入力 ---
 st.subheader("測定値入力")
